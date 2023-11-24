8eeb7c63a3b69a43fe68e66c1e7848e95eda44f7
Tom Hockett <tom.hockett@gmail.com>
2023-11-24 11:00:28 -0600

    init repo

---
+++ lua/plugins/sonokai.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "sainnhe/sonokai",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      vim.g["sonokai_style"] = "shusia"
+      vim.g["sonokai_transparent_background"] = 1
+    end,
+  },
+}
