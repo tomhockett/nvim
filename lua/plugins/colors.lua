7701e7d1a57d57fa779a717b75853d30c75e61d4
Tom Hockett <tom.hockett@gmail.com>
2024-06-04 08:29:38 -0500

    updatessss

--- lua/plugins/colors.lua
+++ lua/plugins/colors.lua
@@ -32,4 +32,18 @@ return {
       })
     end,
   },
+  {
+    "sainnhe/gruvbox-material",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      -- Optionally configure and load the colorscheme
+      -- directly inside the plugin declaration.
+      vim.g.gruvbox_material_enable_italic = true
+      -- vim.cmd.colorscheme('gruvbox-material')
+    end,
+  },
+  {
+    "savq/melange-nvim",
+  },
 }
--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -2,7 +2,7 @@ return {
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "monokai-pro",
+      colorscheme = "gruvbox-material",
     },
   },
 }
